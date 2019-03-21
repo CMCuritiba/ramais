3c19dd78fa4df5a9b2b51fd8453ac7bf56aaa16c
aleodoni <aleodoni@gmail.com>
2019-03-21 12:02:32 -0300

    melhorias cosmeticas e filtro da setor e funcionario

--- frontend/ramais/pages/index.vue
+++ frontend/ramais/pages/index.vue
@@ -1,14 +1,59 @@
 <template>
-<div>
-  <Tabela />
-</div>
+<v-card>
+  <v-card-text>
+    <v-text-field box label="Digite o funcionário ou departamento" append-icon="search" id="textoFiltro" v-model="textoFiltro"></v-text-field>
+    <v-layout row wrap>
+      <v-flex md12 xs12>
+        <v-card :color="corInfo" class="white--text">
+          <v-card-title primary-title>
+            {{ informacao }}. Para baixar o PDF da lista, clique&nbsp;<b>AQUI</b>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card-text>
+  <Tabela :listaFiltrada="ramaisFiltrados"/>
+</v-card>
 </template>
 
 <script>
-import Tabela from '~/components/UI/Tabela.vue'
+import Tabela from '~/components/UI/Tabela.vue';
+
 export default {
   components: {
-    Tabela
+    Tabela,
+  },
+  data() {
+    return {
+      textoFiltro: ''
+    }
+  },
+  computed: {
+    lista() {
+      return this.$store.getters.ramais;
+    },
+    ramaisFiltrados() {
+      //return this.lista;      
+      const filter = new RegExp(this.textoFiltro, 'i');
+      return this.lista.filter(el => {
+        if ((el['set_nome'].match(filter)) || (el['pes_nome'].match(filter)))
+          return true;
+        else
+          return false;
+      })
+    },
+    informacao() {
+      if (this.textoFiltro != '')
+        return 'Filtrando lista por : ' + this.textoFiltro;
+      else
+        return 'Lista completa';
+    },
+    corInfo() {
+      if (this.textoFiltro != '')
+        return 'red darken-3';
+      else
+        return 'blue-grey darken-1';
+    }
   },
 
 }
