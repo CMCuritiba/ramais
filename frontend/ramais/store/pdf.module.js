e9765df497b764fd24cddf66c72da371e70eaa38
aleodoni <aleodoni@gmail.com>
2019-06-18 17:09:57 -0300

    report workaround

--- frontend/ramais/store/pdf.module.js
+++ frontend/ramais/store/pdf.module.js
@@ -1,50 +1,58 @@
 import axios from 'axios'
 
-import { 
-    LISTA_RAMAIS_PDF_URL, 
-    AXIOS_HEADER, 
-    IS_LOADING,
-    DONE_LOADING
-} from "./constants.js";
+import {
+  LISTA_RAMAIS_PDF_URL,
+  AXIOS_HEADER,
+  IS_LOADING,
+  DONE_LOADING
+} from './constants.js'
 
 const state = {
-    loading: false
-};
+  loading: false
+}
 
 const getters = {
-  loading(state) {
-      return state.loading;
+  loading (state) {
+    return state.loading
   }
-};  
+}
 
 const actions = {
 
-    async geraRelatorio({ commit }, parametros) {
-      commit(IS_LOADING)
-      let res = await axios.get(LISTA_RAMAIS_PDF_URL + parametros)
-      var blob = new Blob([res.data], {
-        type: 'application/pdf'
-      });
-      var url = window.URL.createObjectURL(blob)
-      window.open(url, 'relatorio')
-      commit(DONE_LOADING)
-    }
-};
+  async geraRelatorio ({ commit }, parametros) {
+    commit(IS_LOADING)
+    let res = await axios.get(LISTA_RAMAIS_PDF_URL + parametros)
+    var blob = new Blob([res.data], {
+      type: 'application/pdf'
+    })
+    var url = window.URL.createObjectURL(blob)
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.style = 'display: none'
+    a.href = url
+    a.download = 'lista_de_ramais.pdf'
+    a.target = '_relatorio'
+    // window.open(url, 'relatorio')
+    // window.URL.revokeObjectURL(url)
+    a.click()
+    commit(DONE_LOADING)
+  }
+}
 
 const mutations = {
 
-    [IS_LOADING](state) {
-        state.loading = true
-    },
+  [IS_LOADING] (state) {
+    state.loading = true
+  },
 
-    [DONE_LOADING](state) {
-        state.loading = false
-    }
-};
+  [DONE_LOADING] (state) {
+    state.loading = false
+  }
+}
 
 export default {
-    state,
-    actions,
-    mutations,
-    getters
-};
+  state,
+  actions,
+  mutations,
+  getters
+}
